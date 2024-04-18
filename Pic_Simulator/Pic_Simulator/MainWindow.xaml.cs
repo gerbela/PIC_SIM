7a1144c715acae62dfc4e48efe5168320f147b67
robbebee <ronni.mein@gmail.com>
2024-04-18 13:34:02 +0200

    set correct start Position in Textbox

--- Pic_Simulator/Pic_Simulator/MainWindow.xaml.cs
+++ Pic_Simulator/Pic_Simulator/MainWindow.xaml.cs
@@ -13,11 +13,12 @@ namespace Pic_Simulator
     /// </summary>
     public partial class MainWindow : Window
     {
-        int[,] ram = new int[2,128];
+        int[,] ram = new int[2, 128];
         List<int> commands = new List<int>();
         int pos = 0;
         bool loadedFile = false;
         int wReg = 0;
+        int startPos;
         public MainWindow()
         {
             InitializeComponent();
@@ -29,40 +30,33 @@ namespace Pic_Simulator
             dialog.Filter = "Text documents (.lst)|*.lst";
             bool? result = dialog.ShowDialog();
 
-            if(result == true) 
+            if (result == true)
             {
                 int counter = 0x0000;
-                bool reachedCommands = false;
                 int pos = 1;
 
-                foreach (string s in File.ReadLines(dialog.FileName)) 
+                foreach (string s in File.ReadLines(dialog.FileName))
                 {
                     string file = "";
-                    /*if (!s.StartsWith("0000") && reachedCommands == false)
+
+                    string firstFour = s.Substring(0, 4);
+                    if (s.Substring(0, 4) == "    ")
                     {
                         string tmp = "        " + s;
                         file = file + tmp;
-                    }*/
-                    
-                        reachedCommands = true;
-
-                        string firstFour = s.Substring(0, 4);
-                        if (s.Substring(0, 4) == "    ")
+                    }
+                    else
+                    {
+                        firstFour = "0x" + firstFour;
+                        int value = Convert.ToInt32(firstFour, 16);
+                        string command = "0x" + s.Substring(5, 4);
+                        if (value == counter)
                         {
-                            string tmp = "        " + s;
-                            file = file + tmp;
+                            if (value == 0) startPos = pos;
+                            commands.Add(Convert.ToInt32(command, 16));
+                            file = file + s;
+                            counter++;
                         }
-                        else
-                        {
-                            firstFour = "0x" + firstFour;
-                            int value = Convert.ToInt32(firstFour, 16);
-                            string command = "0x" + s.Substring(5,4);
-                            if (value == counter)   
-                            {
-                                commands.Add(Convert.ToInt32(command,16));
-                                file = file + s;
-                                counter++;
-                            }
                     }
                     TextBox textBox = new TextBox();
                     textBox.Text = file;
@@ -72,8 +66,10 @@ namespace Pic_Simulator
                     pos++;
 
                 }
-                foreach (int i in commands) Result.Text = Result.Text + i + "\n";
+                //print commands
+                //foreach (int i in commands) Result.Text = Result.Text + i + "\n";
                 loadedFile = true;
+                MarkLine();
             }
         }
 
@@ -82,11 +78,18 @@ namespace Pic_Simulator
             MarkLine();
             int command = Fetch();
             Decode(command);
+            Result.Text = "";
+            //print ram
+            /*for(int i = 0; i < 128; i++)
+            {
+                Result.Text = Result.Text + " " + ram[0, i];
+            }
+            Result.Text = Result.Text + "\n" + "W-Register: " + wReg;*/
         }
 
         private int Fetch()
         {
-            int programCounter = ram[0,2];
+            int programCounter = ram[0, 2];
             int command = commands[programCounter];
             programCounter++;
             ram[0, 2] = programCounter;
@@ -95,20 +98,36 @@ namespace Pic_Simulator
 
         private void Decode(int command)
         {
-            if((command &  0x3F00) == 0x3000)
+            if ((command & 0x3F00) == 0x3000)
             {
-                MOVLW(command & 0xFF); 
+                MOVLW(command & 0xFF);
             }
-        }
             if ((command & 0xFFA0) == 0x00A0)
             {
-                MovWF(command & Convert.ToInt32("7F",16));
+                MovWF(command & Convert.ToInt32("7F", 16));
+            }
+            if((command & 0x3FA0) == 0x09A0 || (command & 0x3FA0) == 0x0900)
+            {
+                ADDWF(command & 0xFF);
             }
         }
 
+        private void ADDWF(int address)
+        {
+            int result = ram[0, address & 0x007F] + wReg;
+            if((address & 0x00A0) == 0x00A0)
+            {
+                ram[0, address & 0x007F] = result;
+            }
+            else
+            {
+                wReg = result;
+            }
+        }
         private void MOVLW(int literal)
         {
-            wReg = literal; 
+            wReg = literal;
+        }
 
         private void MovWF(int storageLocation)
         {
@@ -118,17 +137,22 @@ namespace Pic_Simulator
         private void MarkLine()
         {
             if (!loadedFile) return;
+            if (pos == 0)
+            {
+                pos = startPos - 2;
+                CodeScroller.ScrollToVerticalOffset(CodeScroller.VerticalOffset + 25*(startPos-4));
+            }
             TextBox text = (TextBox)Stack.Children[pos];
             text.Background = Brushes.White;
             pos++;
             if (Stack.Children.Count <= pos)
             {
-                pos = 0;
+                pos = startPos - 2;
                 CodeScroller.ScrollToVerticalOffset(0);
             }
             text = (TextBox)Stack.Children[pos];
             text.Background = Brushes.OrangeRed;
-            if (pos != 0) CodeScroller.ScrollToVerticalOffset(CodeScroller.VerticalOffset + 25);
+            if (pos != startPos-1) CodeScroller.ScrollToVerticalOffset(CodeScroller.VerticalOffset + 25);
         }
     }
 }
