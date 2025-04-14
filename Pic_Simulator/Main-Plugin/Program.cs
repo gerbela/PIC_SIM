4dbffcd4487c073cc798168e895b3d315270fa70
Tobi Friedrich <ronni.mein@gmail.com>
2025-04-14 15:39:24 +0200

    starting wpf from outside

--- Pic_Simulator/Main-Plugin/Program.cs
+++ Pic_Simulator/Main-Plugin/Program.cs
@@ -1,10 +1,17 @@
 ﻿using Pic_Simulator;
 public class Mainprogramm
 {
+
+    [STAThread]
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
 
         var app = new App();
+        //app.InitializeComponent();
+        var mainWindow = new MainWindow();
+        app.Run(mainWindow);
     }
 }
+
+
